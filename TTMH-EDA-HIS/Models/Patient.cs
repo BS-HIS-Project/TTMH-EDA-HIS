575f2573a8b49aed9768d6c0495254f431cd4475
NickLu <word4205@gmail.com>
2023-05-31 10:54:58 +0800

    新增_Model對應SQL註解

--- TTMH-EDA-HIS/Models/Patient.cs
+++ TTMH-EDA-HIS/Models/Patient.cs
@@ -1,26 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace TTMH_EDA_HIS.Models;
 
 public partial class Patient
 {
+    // ID
     public string PatientId { get; set; } = null!;
-
+    // 健保卡號
     public string? Nhicard { get; set; }
-
+    // 病例編號
     public string CaseHistory { get; set; } = null!;
-
+    // 姓名
     public string PatientName { get; set; } = null!;
-
+    // 出生年月日
     public DateTime BirthDate { get; set; }
-
+    // 性別
     public string Gender { get; set; } = null!;
-
+    // 血型
     public string? Blood { get; set; }
-
+    // 居住地址
     public string Address { get; set; } = null!;
-
+    // 電話
     public string Mobile { get; set; } = null!;
 
     public virtual ICollection<Detail> Details { get; set; } = new List<Detail>();
--- TTMH-EDA-HIS/Models/Pharmacist.cs
+++ TTMH-EDA-HIS/Models/Pharmacist.cs
@@ -5,8 +5,8 @@ namespace TTMH_EDA_HIS.Models;
 
 public partial class Pharmacist
 {
+    // ID
     public string PhaId { get; set; } = null!;
-
     public virtual Employee Pha { get; set; } = null!;
 
     public virtual ICollection<Prescription> Prescriptions { get; set; } = new List<Prescription>();
