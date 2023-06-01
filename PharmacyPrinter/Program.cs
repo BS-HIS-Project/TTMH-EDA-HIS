b883ce129bd571bc938bce43187077fd9ccfb20e
NickLu <word4205@gmail.com>
2023-06-01 17:34:21 +0800

    提交_怪怪的東西

---
+++ PharmacyPrinter/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+
+ConsumerConfig config = new ConsumerConfig
+{
+    BootstrapServers = "server.nicklu89.com:9092",
+    GroupId = "test-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("my-topic");
+    //consumer.OffsetsForTimes();
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+
+        Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+    }
+}
