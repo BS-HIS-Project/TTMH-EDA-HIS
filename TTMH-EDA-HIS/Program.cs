10a74e582651d2500db539eb7c9471a9207de9e6
althea <azhong531@gmail.com>
2023-06-06 16:33:35 +0800

    employee controller&view 刪除

--- TTMH-EDA-HIS/Program.cs
+++ TTMH-EDA-HIS/Program.cs
@@ -10,11 +10,18 @@ builder.Services.AddDbContext<HisdbContext>(
     options => options.UseSqlServer(conn)
 );
 
+//builder.Services.AddControllers();
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
+
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
---
+++ kafka_test_1/kafka_test_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Confluent.Kafka;
+using System.Text.RegularExpressions;
+
+ConsumerConfig config = new ConsumerConfig
+{
+    BootstrapServers = "server.nicklu89.com:9092",
+    GroupId = "test-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+    
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("my-topic");
+
+    while(true)
+    {
+        var consumeResult = consumer.Consume();
+        Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+    }
+}
