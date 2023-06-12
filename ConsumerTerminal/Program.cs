066179196575be48e6b5f8c60c141918cbf0cb18
NickLu <word4205@gmail.com>
2023-06-12 20:41:35 +0800

    新增_ConsumerTerminal

---
+++ ConsumerTerminal/Program.cs
@@ -0,0 +1,25 @@
+﻿using Confluent.Kafka;
+using HISDB.Models;
+using HISDB.Data;
+using HISDB;
+
+var _context = new HisdbContext();
+
+ConsumerConfig config = new ConsumerConfig
+{
+    BootstrapServers = "server.nicklu89.com:9092",
+    GroupId = "test-group",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+{
+    consumer.Subscribe("my-topic");
+    //consumer.OffsetsForTimes();
+    while (true)
+    {
+        var consumeResult = consumer.Consume();
+
+        Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+    }
+}
