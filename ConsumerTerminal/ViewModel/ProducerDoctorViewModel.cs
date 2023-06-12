c76278b2fe23ac8ae5e80cdab956aee4c32aedcc
NickLu <word4205@gmail.com>
2023-06-12 17:25:51 +0800

    求救_上傳

---
+++ ConsumerTerminal/ViewModel/ProducerDoctorViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerTerminal.ViewModel
+{
+    public class DoctorMessage
+    {
+        public string? DoctorId { get; set; }
+        public string? PatientID { get; set; }
+        public string? ChaID { get; set; }
+
+        public List<ChartsDrugsDosages>? ChartsDrugsDosages { get; set; }
+    }
+
+    public class ChartsDrugsDosages
+    {
+
+        public string DrugId { get; set; } = null!;
+
+        public string DosId { get; set; } = null!;
+
+        public int Days { get; set; }
+
+        public string? Remark { get; set; }
+    }
+}
