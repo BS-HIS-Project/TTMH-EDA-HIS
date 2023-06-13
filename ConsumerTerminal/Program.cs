e930bbddb85946c5c78d50a1e14cd4cf9591e126
NickLu <word4205@gmail.com>
2023-06-13 11:04:28 +0800

    加入_自訂GroupID

--- ConsumerTerminal/Program.cs
+++ ConsumerTerminal/Program.cs
@@ -7,10 +7,13 @@ using HISDB;
 
 var _context = new HisdbContext();
 
+Console.WriteLine("請輸入GroupId");
+var inputGroupId = Console.ReadLine();
+
 ConsumerConfig config = new ConsumerConfig
 {
     BootstrapServers = "server.nicklu89.com:9092",
-    GroupId = "Group001",
+    GroupId = inputGroupId,
     AutoOffsetReset = AutoOffsetReset.Earliest
 };
 
@@ -30,23 +33,29 @@ using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
         {
             var JsonData = JsonSerializer.Deserialize<DoctorMessage>(consumeResult.Message.Value);
 
-            Detail _detail = new Detail()
+            if (JsonData == null)
             {
-                // DET + NOWDATE + 診間號 + (繳費條碼)序號
-                DetId = $"DET{DateTime.Now.ToString("yyyyMMddHHmmss")}{ClinicNumber.ToString().PadLeft(3, '0')}{PaymentBarcode.ToString().PadLeft(3, '0')}",
-                // 掛號費
-                Registration = Convert.ToDecimal(150),
-                // 藥費
-                MedicalCost = Convert.ToDecimal(500),
-                // 應繳金額
-                Payable = Convert.ToDecimal(650),
+                Console.WriteLine("Msg not DoctorMessage or JsonData is NULL");
+            } else
+            {
+                Detail _detail = new Detail()
+                {
+                    // DET + NOWDATE + 診間號 + (繳費條碼)序號
+                    DetId = $"DET{DateTime.Now.ToString("yyyyMMddHH")}{ClinicNumber.ToString().PadLeft(3, '0')}{PaymentBarcode.ToString().PadLeft(3, '0')}",
+                    // 掛號費
+                    Registration = Convert.ToDecimal(150),
+                    // 藥費
+                    MedicalCost = Convert.ToDecimal(500),
+                    // 應繳金額
+                    Payable = Convert.ToDecimal(650),
 
-                CasId = CashierId,
-                PatientId = "A118992634"
-            };
-            PaymentBarcode++;
-            _context.Details.Add(_detail);
-            _context.SaveChanges();
+                    CasId = CashierId,
+                    PatientId = JsonData.PatientID ?? throw new Exception("PatientId is NULL")
+                };
+                PaymentBarcode++;
+                _context.Details.Add(_detail);
+                _context.SaveChanges();
+            }
         }
         catch (Exception e)
         {
