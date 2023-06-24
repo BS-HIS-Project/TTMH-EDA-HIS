4d40ac8bc3c1aee9b52fdd6a8d2f857eb711dd32
Lin Chi Kin <birbchurchofficial@gmail.com>
2023-06-25 01:40:55 +0800

    新增_SweetAlert2 回應Message

---
+++ TTMH-EDA-HIS/Controllers/CPOEsCommitAPIController.cs
@@ -0,0 +1,49 @@
+﻿using HISDB.Data;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TTMH_EDA_HIS.ViewModels;
+
+namespace TTMH_EDA_HIS.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CPOEsCommitAPIController : ControllerBase
+    {
+        private readonly HisdbContext _context;
+        public CPOEsCommitAPIController(HisdbContext context)
+        {
+            _context = context;
+        }
+
+        [Authorize]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> CommitDrugs(CPOEsCommitAPICommitDrugsViewModel vm)
+        {
+            
+
+           
+
+            return Ok(new {
+                Status="Success",
+                message=""
+            });
+        }
+
+
+        [Authorize]
+        [HttpPost("[action]")]
+        public async Task<IActionResult> GeneratePrescription()
+        {
+
+
+
+            return Ok(new{
+                Status = "Success",
+                message = ""
+            });
+        }
+
+
+
+    }
+}
