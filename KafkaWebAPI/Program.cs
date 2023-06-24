4c843ff9b73b4bb37ffa9c20a699da3bee2c70c6
NickLu <word4205@gmail.com>
2023-06-24 14:57:18 +0800

    新增_測試用GET

--- KafkaWebAPI/Program.cs
+++ KafkaWebAPI/Program.cs
@@ -5,7 +5,10 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new() { Title = "KafkaWebAPI", Version = "v1" });
+});
 
 var app = builder.Build();
 
