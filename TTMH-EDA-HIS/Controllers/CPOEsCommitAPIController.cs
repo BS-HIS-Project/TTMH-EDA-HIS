78efa70a721b91479949b5bd6510f57d5f442333
Lin Chi Kin <birbchurchofficial@gmail.com>
2023-06-26 14:27:48 +0800

    新增 PatientDetails_View/ReprintPrescription()

--- TTMH-EDA-HIS/Controllers/CPOEsCommitAPIController.cs
+++ TTMH-EDA-HIS/Controllers/CPOEsCommitAPIController.cs
@@ -4,6 +4,8 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Data;
 using TTMH_EDA_HIS.ViewModels;
+using System.Text.Json;
+using System.Text;
 
 namespace TTMH_EDA_HIS.Controllers
 {
@@ -23,6 +25,7 @@ namespace TTMH_EDA_HIS.Controllers
         {
             Doctor? doctor = new Doctor();
             Patient? patient = new Patient();
+            Chart chart = new Chart();
 
             try
             {
@@ -32,16 +35,20 @@ namespace TTMH_EDA_HIS.Controllers
                 {
                     return Ok(new
                     {
-                        Status = "NotFound",
-                        Message = "Patient Not Found"
+                        Icon = "question",
+                        Title = "Not Found",
+                        Text = "Patient Not Found",
+                        Details = ""
                     });
                 }
                 if (doctor == null)
                 {
                     return Ok(new
                     {
-                        Status = "NotFound",
-                        Message = "Doctor Not Found"
+                        Icon = "question",
+                        Title = "Not Found",
+                        Text = "Doctor Not Found",
+                        Details = ""
                     });
                 }
 
@@ -54,7 +61,7 @@ namespace TTMH_EDA_HIS.Controllers
                 string minStr = TwoUnitNumericString(DateTime.Now.Minute);
 
                 string cid = $"CHA{yearStr}{monthStr}{dayStr}{hourStr}{minStr}";
-                Chart chart = new Chart()
+                chart = new Chart()
                 {
                     ChaId = cid,
                     DepartmentName = doctor.DepartmentName ?? "",
@@ -64,7 +71,6 @@ namespace TTMH_EDA_HIS.Controllers
                     History = ""
                 };
                 await _context.Charts.AddAsync(chart);
-
 
 
                 DoctorsPatientsChart dpc = new DoctorsPatientsChart()
@@ -93,35 +99,91 @@ namespace TTMH_EDA_HIS.Controllers
 
                 await _context.SaveChangesAsync();
 
-
-                return Ok(new
-                {
-                    Status = "Success",
-                    Message = ""
-                });
             }
             catch (Exception ex)
             {
                 return Ok(new
                 {
-                    Status = "Error",
-                    Message = ex.Message,
-                    Details = ex.InnerException.ToString()
+                    Icon = "error",
+                    Title = "Error",
+                    Text = ex.Message,
+                    Details = ex.InnerException.Message
+                });
+            }
+            try
+            {
+                CPOEsCommitAPIPrescriptionViewModel pvm = new CPOEsCommitAPIPrescriptionViewModel()
+                {
+                    Topic = "",
+                    Key = "",
+                    Message = new CPOEsCommitAPIPrescriptionViewModel_DoctorMessage()
+                    {
+                        DoctorId = vm.DoctorID,
+                        PatientId = vm.PatientID,
+                        ChaId = chart.ChaId,
+                        ChartsDrugsDosages = new List<CPOEsCommitAPIPrescriptionViewModel_ChartsDrugsDosage>()
+                    }
+                };
+                foreach (CPOEsCommitAPICommitDrugsViewModel_Drugs i in vm.Drugs)
+                {
+                    pvm.Message.ChartsDrugsDosages.Add(new CPOEsCommitAPIPrescriptionViewModel_ChartsDrugsDosage()
+                    {
+                        DrugId = i.DrugID,
+                        DosId = i.DosID,
+                        Quantity = double.Parse(i.Quantity),
+                        Days = int.Parse(i.Days),
+                        Total = int.Parse(i.Total),
+                        Remark = i.Remark
+                    });
+                }
+
+                string response = await PrintPrescription(pvm);
+                if (response == null) { 
+                    throw new Exception("Connection Failed"); 
+                }
+
+                return Ok(new
+                {
+                    Icon = "success",
+                    Title = "成功上存",
+                    Text = response,
+                    Details = ""
+                });
+            }
+            catch (Exception ex) 
+            {
+                return Ok(new
+                {
+                    Icon = "warning",
+                    Title = "Warning",
+                    Text = "Database Inserted Success but failed to generate prescription",
+                    Details = ex.Message + "\n" + ex.InnerException.Message
                 });
             }
         }
 
-        [Authorize]
-        [HttpPost("[action]")]
-        public async Task<IActionResult> GeneratePrescription()
+        private async Task<string> PrintPrescription(CPOEsCommitAPIPrescriptionViewModel vm)
         {
+            try
+            {
+                string? responseJsonStr = null;
+                string postJsonStr = JsonSerializer.Serialize(vm);
 
+                StringContent content = new StringContent(postJsonStr, Encoding.UTF8, "application/json");
 
+                HttpClient client = new HttpClient();
+                HttpResponseMessage response = await client.PostAsync("url", content);
 
-            return Ok(new{
-                Status = "Success",
-                Message = ""
-            });
+                if (response.IsSuccessStatusCode)
+                {
+                    responseJsonStr = await response.Content.ReadAsStringAsync();
+                }
+                return responseJsonStr;
+            }
+            catch (Exception ex)
+            {
+                return ex.InnerException.Message;
+            }
         }
 
         private string TwoUnitNumericString(int value)
