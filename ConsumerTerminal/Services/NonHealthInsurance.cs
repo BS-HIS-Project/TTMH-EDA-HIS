88739dce56d61d08b8a5eb21a35b489d8ecc1d4d
NickLu <word4205@gmail.com>
2023-06-29 17:54:03 +0800

    修改_將共用部分提升致父曾

--- ConsumerTerminal/Services/NonHealthInsurance.cs
+++ ConsumerTerminal/Services/NonHealthInsurance.cs
@@ -10,16 +10,12 @@ namespace ConsumerTerminal.Services
     public class NonHealthInsurance : Billing
     {
         private readonly HisdbContext _context;
-        private string PatientId { get; set; }
-        private string ChargeId { get; set; }
 
         private PartialServices _partialServices { get; set; }
-        public NonHealthInsurance(string PatientId, string ChargeId) : base()
+        public NonHealthInsurance(string PatientId) : base(PatientId)
         {
             _context = new HisdbContext();
 
-            this.PatientId = PatientId;
-            this.ChargeId = ChargeId;
 
             if (ChackPatient())
             {
@@ -32,55 +28,9 @@ namespace ConsumerTerminal.Services
 
         }
 
-        private bool ChackPatient()
-        {
-            var _patient = _context.Patients.Where(p => p.PatientId == PatientId).FirstOrDefault();
-            if (_patient == null)
-            {
-                return false;
-            }
-            else
-            {
-                return true;
-            }
-        }
         public override Decimal DrugFee()
         {
             return 0;
-        }
-
-        public override Decimal PartialPayment()
-        {
-            int age = _partialServices.GetAge();
-
-            if (age < 3) return 0;
-            else return 240;
-        }
-
-        public override Decimal RegistrationFee()
-        {
-            Decimal temp = 0;
-            Decimal total = 0;
-
-            if (DateTime.Now.DayOfWeek != DayOfWeek.Saturday || DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
-            {
-                temp = 30;
-            }
-            else if (DateTime.Now.Hour >= 18)
-            {
-                temp = 30;
-            }
-
-            if (_partialServices.GetAge() >= 70)
-            {
-                total = 100;
-            }
-            else
-            {
-                total = 150;
-            }
-
-            return total - temp;
         }
 
         public override Decimal DiagnosticFee()
