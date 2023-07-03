272f38883ac825caa98f4c12fe91200568c21b73
NickLu <word4205@gmail.com>
2023-07-03 13:59:04 +0800

    新增_藥袋第一頁

---
+++ ConsumerTerminal/Services/PrintSystem/PrescriptionServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsumerTerminal.Services.PrintSystem
+{
+    public class PrescriptionServices : DataProcessing
+    {
+        public override void ChangeData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void InputHTML(string FileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void OutputPDF(string FileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
