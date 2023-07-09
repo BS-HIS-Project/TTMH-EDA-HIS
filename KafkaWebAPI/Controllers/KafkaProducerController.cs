61d0b71afa859616983c574813670eed6a608c08
NickLu <word4205@gmail.com>
2023-07-09 22:55:39 +0800

    修正_API傳入內容

--- KafkaWebAPI/Controllers/KafkaProducerController.cs
+++ KafkaWebAPI/Controllers/KafkaProducerController.cs
@@ -37,7 +37,7 @@ namespace KafkaWebAPI.Controllers
         public IActionResult KafkaProducerDetail([FromBody] KafkaProducerDetailViewModel value)
         {
             KafkaProducer controller = new KafkaProducer("server.nicklu89.com:9092");
-            controller.Produce(value.Topic ?? "no-topic", value.Key ?? "no-key", System.Text.Json.JsonSerializer.Serialize(value.DetId) ?? "no-message"); // topic, message
+            controller.Produce(value.Topic ?? "no-topic", value.Key ?? "no-key", System.Text.Json.JsonSerializer.Serialize(value.Message) ?? "no-message"); // topic, message
             return Ok();
         }
     }
--- KafkaWebAPI/ViewModels/KafkaProducerDetailViewModel.cs
+++ KafkaWebAPI/ViewModels/KafkaProducerDetailViewModel.cs
@@ -4,6 +4,13 @@
     {
         public string? Topic { get; set; }
         public string? Key { get; set; }
-        public string? DetId { get; set; }
+        public Receipt? Message { get; set; }
+    }
+
+    public class Receipt
+    {
+        public string DetId { get; set; } = null!;
+        public string? PatientId { get; set; }
+        public string? Vdate { get; set; }
     }
 }
